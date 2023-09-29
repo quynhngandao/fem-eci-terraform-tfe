5c76600f13f0a86ae7d01c1639de994d9b3fb9dc
Quynh Ngan Dao <nguy2884@umn.edu>
2023-09-29 14:32:47 -0500

    feat: setup

--- locals.tf
+++ locals.tf
@@ -7,10 +7,17 @@ locals {
 
   workspace = {
     "fem-eci-tfe" = {
-      description         = "Example description of workspace"
+      description         = "Example automation workspace for Terraform Cloud resources."
       execution_mode      = "remote"
       project_id          = module.project["fem-eci-project"].id
-      vcs_repo_identifier = "quynhngandao/fem-eci-terraform-tfe"
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
+    }
+
+    "fem-eci-github" = {
+      description         = "Example automation workspace for GitHub resources."
+      execution_mode      = "local"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
   }
 }
