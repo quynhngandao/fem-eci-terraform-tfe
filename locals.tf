7ef55fa470418a6e0a35e5c740b0fea7b8ba5a7e
Quynh Ngan Dao <nguy2884@umn.edu>
2023-09-30 22:06:23 -0500

    feat: added workspaces

--- locals.tf
+++ locals.tf
@@ -19,5 +19,63 @@ locals {
       project_id          = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
+
+    "fem-eci-aws-network" = {
+      description         = "Automation for AWS network resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-aws-network"
+
+# config workspaces 
+      variables = [
+        {
+          category = "terraform"
+          hcl      = true # array parsing 
+          key      = "azs"
+          value    = jsonencode(["us-west-2a", "us-west-2b"])
+        },
+        {
+          category = "terraform"
+          key      = "cidr"
+          value    = "10.0.0.0/16"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci"
+        },
+      ]
+    }
+
+# need to be specifc with name of cluster 
+    "fem-eci-aws-cluster" = {
+      description         = "Automation for AWS cluster resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-aws-cluster"
+
+      variables = [
+        {
+          category = "terraform"
+          key      = "domain"
+          value    = "quynhdao.dev" 
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci-quynhdao" # name of cluster, need to be unique 
+        },
+        {
+          category = "terraform"
+          key      = "vpc_name"
+          value    = "fem-eci" # 
+        },
+      ]
+    }
   }
 }
