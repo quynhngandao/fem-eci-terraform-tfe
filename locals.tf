0a0a13a3eec8cbb4f199caf07275d44ad0803981
Quynh Ngan Dao <nguy2884@umn.edu>
2023-09-30 23:04:24 -0500

    feat: added workspaces

--- locals.tf
+++ locals.tf
@@ -5,6 +5,7 @@ locals {
     }
   }
 
+
   workspace = {
     "fem-eci-tfe" = {
       description         = "Example automation workspace for Terraform Cloud resources."
@@ -13,6 +14,7 @@ locals {
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
     }
 
+
     "fem-eci-github" = {
       description         = "Example automation workspace for GitHub resources."
       execution_mode      = "remote"
@@ -20,17 +22,18 @@ locals {
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
 
+
     "fem-eci-aws-network" = {
       description         = "Automation for AWS network resources."
       execution_mode      = "remote"
       project_id          = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-aws-network"
 
-# config workspaces 
+
       variables = [
         {
           category = "terraform"
-          hcl      = true # array parsing 
+          hcl      = true # array parsing
           key      = "azs"
           value    = jsonencode(["us-west-2a", "us-west-2b"])
         },
@@ -46,19 +49,19 @@ locals {
         },
       ]
     }
-
-# need to be specifc with name of cluster 
-    "fem-eci-aws-cluster" = {
+    # need to be specifc with name of cluster
+    "fem-eci-aws-cluster-prod" = {
       description         = "Automation for AWS cluster resources."
       execution_mode      = "remote"
       project_id          = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-aws-cluster"
 
+
       variables = [
         {
           category = "terraform"
           key      = "domain"
-          value    = "quynhdao.dev" 
+          value    = "daoquynh29.dev"
         },
         {
           category = "terraform"
@@ -68,14 +71,15 @@ locals {
         {
           category = "terraform"
           key      = "name"
-          value    = "fem-eci-quynhdao" # name of cluster, need to be unique 
+          value    = "fem-eci-quynhdao"
         },
         {
           category = "terraform"
           key      = "vpc_name"
-          value    = "fem-eci" # 
+          value    = "fem-eci"
         },
       ]
     }
   }
 }
+
